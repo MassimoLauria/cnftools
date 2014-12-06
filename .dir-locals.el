8e07c141699227a9bbf9f03a7913d0ec0a6ba2e7
Massimo Lauria <lauria.massimo@gmail.com>
2014-12-06 18:50:05 +0100

    Emacs project configuration.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c++-mode . ((flycheck-clang-language-standard . "c++11")
+              (flycheck-gcc-language-standard . "c++11")))
+ (c-mode   . ((flycheck-clang-language-standard . "c99")
+              (flycheck-gcc-language-standard . "c99"))))
